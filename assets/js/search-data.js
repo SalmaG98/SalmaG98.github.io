b081407f85818ee62de265e84f2bceb28e0213d9
SalmaG98 <53817405+SalmaG98@users.noreply.github.com>
2026-05-26 13:11:30 +0000

    Deploying to gh-pages from @ SalmaG98/SalmaG98.github.io@3e470d80876bed73068764d3d638f57b7ee4d781 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,16 +398,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-will-be-contributing-to-a-tutorial-cvpr-2026-related-to-our-preprint-3d-human-pose-and-shape-estimation-from-lidar-point-clouds-a-review",
+          title: 'I will be contributing to a tutorial @CVPR 2026 related to our preprint...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
